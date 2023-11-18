8a747c93f20f8c861fae935b7fb09ca71c13d671
dzaki201 <muhdzaki206@gmail.com>
2023-11-18 13:33:13 +0700

    Tugas

---
+++ jobsheet5/models/Mahasiswa.php
@@ -0,0 +1,14 @@
+<?php
+
+class mahasiswa {
+    private $koneksi;
+    public function __construct($db) {
+        $this->koneksi = $db;
+    }
+
+    public function getAllMahasiswa() {
+        $query = "SELECT * FROM mahasiswa";
+        $result = mysqli_query($this->koneksi, $query); 
+        return $result;
+    }
+}
